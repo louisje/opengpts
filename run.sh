c36231fbe31df3ed68453fc0b97931928cafb1b5
Louis Jeng <louisje@gmail.com>
2023-12-28 12:04:29 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-docker image prune -f \
+containers="$(docker ps -aq)"
+if [ -n "$containers" ]; then
+    docker stop $containers
+fi
+
+docker container prune -f \
+    && docker image prune -f \
     && docker-compose up --build "$@"
 
