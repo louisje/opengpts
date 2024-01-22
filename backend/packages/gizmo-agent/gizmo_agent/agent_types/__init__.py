a09b2597d68e29a2f267df493ac3a897afcc82fd
Louis Jeng <louisje@gmail.com>
2024-01-22 01:56:01 +0000

    GizmoAgentType

--- backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
+++ backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
@@ -7,9 +7,9 @@ from .xml.agent import get_xml_agent
 
 
 class GizmoAgentType(str, Enum):
-    GPT_4 = "GPT 4"
-    GPT_35_TURBO = "GPT 3.5 Turbo"
-    FFM = "FFM"
+    GPT_4 = "OpenAI (gpt-4)"
+    GPT_35_TURBO = "OpenAI (gpt-3.5-turbo)"
+    FFM = "FFM (ffm-llama2-70b-chat)"
     OLLAMA = "Ollama (llama2-7b-chat)"
     TAIWAN = "Ollama (taiwan-llm-7b-v2.1-chat)"
 
